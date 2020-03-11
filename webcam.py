b880fadf73e1db96aca4d920359318d74bcbf496
tejala2 <55035424+tejala2@users.noreply.github.com>
2020-03-11 17:01:29 -0500

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,1 +1,11 @@
+import cv2
+
+webcamStreamObject = cv2.VideoCapture(0)
+result = True
+while(result):
+    ret,frame = webcamStreamObject.read()
+    cv2.imwrite("NewPic.jpg",frame)
+    result = False
+webcamStreamObject.release()
+cv2.destroyAllWindows()
 
